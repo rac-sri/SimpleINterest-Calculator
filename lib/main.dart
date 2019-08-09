07a19e501e289f55804c65ae84a0950b1f3e606e
rachit2501 <rac.sri25@gmail.com>
2019-08-10 01:23:01 +0530

    needed to backup

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,165 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+void main () {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Simple Interest Calculator App",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      home: SIForm(),
+    )
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SIForm extends StatefulWidget{
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SIFormState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Rupees' , 'Dollars', 'Pounds'];
+  final _minimumPadding = 5.0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    TextStyle textstyle = Theme.of(context).textTheme.title;
+    // TODO: implement build
     return Scaffold(
+      //resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title:Text('Simple Interest Calculator'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body:
+        Container(
+        margin: EdgeInsets.all(_minimumPadding),
+        child : ListView(
+        children:<Widget>[
+            getImageAsset(),
+         Padding(
+
+           padding: EdgeInsets.only(top: _minimumPadding , bottom: _minimumPadding),
+             child: TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Principal',
+              hintText: 'Enter Principal e.g. 12000',
+              labelStyle: textstyle,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5.0)
+              )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
+         ),
+          Padding(
+            padding: EdgeInsets.only(top: _minimumPadding , bottom: _minimumPadding),
+            child:
+          TextField(
+            style : textstyle,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+              labelText: 'Rate of Interest',
+              hintText: 'In percent',
+              labelStyle: textstyle,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0)
+              )
+          ),
         ),
+          ),
+
+        Row(
+          children: <Widget>[
+        Expanded(
+
+        child:
+        TextField(
+          style : textstyle,
+        keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+              labelText: 'Term',
+              hintText: 'Time in Years',
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0)
+              )
+          ),
+        ),
+
+        ),
+
+      Container(width: _minimumPadding*5),
+      Expanded(
+        child:DropdownButton<String>(
+          items: _currencies.map((String value){
+            return DropdownMenuItem<String>(
+              value: value,
+              child : Text(value),
+            );
+          }).toList(),
+
+          value: 'Rupees',
+          onChanged: (String newValueSelected){
+
+          },
+        )
+      )
+
+    ]
+       ),
+      Padding(
+        padding: EdgeInsets.only(bottom: _minimumPadding , top: _minimumPadding),
+        child:Row(children: <Widget>[
+        Expanded(
+
+          child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              textColor: Theme.of(context).primaryColorDark,
+            child: Text('Reset',textScaleFactor: 1.5),
+            onPressed: (){
+
+            }
+          )
+        ),
+          Expanded(
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                  textColor: Theme.of(context).primaryColorDark,
+                  child: Text('Calculate', textScaleFactor: 1.5,),
+                  onPressed: (){
+
+                  }
+              )
+          ),
+
+
+      ],
+        ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          Padding(
+            padding: EdgeInsets.all(_minimumPadding*2),
+            child: Text('Todo Text'),
+          )
+
+
+    ]
+          ),
+        ),
+        );
   }
+
+  Widget getImageAsset()
+  {
+    AssetImage assetImage= AssetImage('images/kirishima_touka_by_miura_n315_dd4rdg7.jpg');
+    Image image = Image(image : assetImage , width: 125.0 , height: 125.0);
+
+    return Container(child: image , margin:EdgeInsets.all(50.0));
+  }
+
 }
